e1fd91e440b3daace9876c0227b504b75097f082
mikeymanoguerra <mikeymanoguerra@gmail.com>
2022-07-10 12:49:51 -0400

    init!

---
+++ init.js
@@ -0,0 +1,76 @@
+window.addEventListener('DOMContentLoaded', () => {
+  initSlider();
+  initSymbolElements();
+  initSymbolRectivity();
+  initDelimeter();
+  validateForm();
+});
+
+function initSymbolElements() {
+  const symbolCodes = [33, 35, 36, 37, 38, 40, 45, 46, 60, 62, 63, 64, 91, 92, 93, 94, 95, 123, 124, 125];
+  const symbolSelect = document.getElementById('symbols');
+
+  symbolCodes.forEach((code) => {
+    const div = document.createElement('div');
+    const checkbox = document.createElement('input');
+    checkbox.textContent = String.fromCharCode(code);
+
+    checkbox.setAttribute('name', 'symbol-' + code);
+    checkbox.setAttribute('id', 'symbol-' + code);
+    checkbox.setAttribute('type', 'checkbox');
+
+    const label = document.createElement('label');
+    label.setAttribute('for', 'symbol-' + code);
+
+    label.textContent = String.fromCharCode(code);
+    div.appendChild(checkbox);
+    div.appendChild(label);
+    symbolSelect.appendChild(div);
+  })
+}
+
+function initSlider() {
+  const lengthDisplay = (value) => document.getElementById('string-length').textContent = value;
+  const lengthInput = document.getElementById('length-input');
+  lengthDisplay(lengthInput.value);
+
+  lengthInput.oninput = event => {
+    lengthDisplay(event.target.value);
+  }
+}
+
+function initSymbolRectivity() {
+  const allSymbols = document.querySelectorAll('.symbols input')
+
+  document.getElementById('select-all').onclick = () => allSymbols.forEach(symbol => symbol.setAttribute('checked', ''));
+  document.getElementById('select-none').onclick = () => allSymbols.forEach(symbol => symbol.removeAttribute('checked'));
+}
+
+function initDelimeter() {
+  const delimeterControls = document.getElementById('delimeter-controls')
+  document.getElementById('yes-delimeter').oninput = () => delimeterControls.removeAttribute('hidden');
+
+  const noDelimeter = document.getElementById('no-delimeter')
+  noDelimeter.oninput = () => delimeterControls.setAttribute('hidden', '');
+  noDelimeter.checked = true
+}
+
+function validateForm() {
+  const setFormValidity = () => {
+    const anyValueSelected = Array.from(checkboxes).some(checkbox => checkbox.checked)
+
+    if (anyValueSelected) {
+      submit.removeAttribute('disabled');
+      return;
+    }
+
+    submit.setAttribute('disabled', '');
+  }
+  // 2022-july: currently checkboxes used to denote value types only. this query would have to change if that is no longer true
+  // at least one checkbox must be checked for the submission script to work
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]')
+  const submit = document.getElementById('submit')
+  checkboxes.forEach(checkbox => checkbox.oninput = () => setFormValidity());
+  setFormValidity();
+
+}
---
+++ submit.js
@@ -0,0 +1,97 @@
+const form = document.getElementById('form');
+form.onsubmit = onSubmit
+
+function onSubmit(event) {
+  event.preventDefault();
+  const params = {};
+
+  for (const [key, value] of new FormData(event.target).entries()) {
+    params[key] = value;
+  }
+
+  document.getElementById('result').textContent = result(params);
+}
+
+function result(params) {
+  const {
+    length,
+    delimeter,
+    useDelimeter
+  } = params;
+
+  if (parseInt(useDelimeter)) {
+    return insertDelimeter(randomString(length, validCharCodes(params)), delimeter);
+  }
+
+  return randomString(length, validCharCodes(params));
+}
+
+function randomString(stringLength, validCharCodes) {
+  let randomCharCodes = new Uint8Array(0);
+
+  while (randomCharCodes.length < stringLength) {
+    const values = window.crypto.getRandomValues(new Uint8Array(stringLength));
+    randomCharCodes = new Uint8Array([...randomCharCodes, ...values.filter(int => validCharCodes.includes(int))]);
+  }
+
+  return String.fromCharCode(...randomCharCodes.slice(0, stringLength));
+}
+
+function insertDelimeter(string, delimeterCode) {
+  const delimeter = String.fromCharCode(delimeterCode);
+
+  let previousDelimeterIndex = 0;
+  let endReached = false
+  while (!endReached) {
+    const interval = intervalFromRange();
+    const front = string.slice(0, previousDelimeterIndex += interval);
+    const back = string.slice(previousDelimeterIndex += 1); //account fo width of interval in string
+
+    if (!back.length) {
+      endReached = true;
+      continue;
+    }
+
+    string = front + delimeter + back;
+  }
+
+  return string;
+}
+
+function intervalFromRange() {
+  const [small, large] = [5, 9]
+  const interval = window.crypto.getRandomValues(new Uint8Array(255)).find(int => (int >= small) && (int <= large));
+
+  return interval ? interval : intervalFromRange();
+}
+
+const numberCodes = range(48, 57);
+const uppercaseCodes = range(65, 90);
+const lowercaseCodes = range(97, 122);
+
+function validCharCodes(params) {
+  params.hasOwnProperty('numbers');
+  params.hasOwnProperty('uppercase');
+  params.hasOwnProperty('lowercase');
+
+  return [
+    ...(params.hasOwnProperty('numbers') ? numberCodes : []),
+    ...(params.hasOwnProperty('uppercase') ? uppercaseCodes : []),
+    ...(params.hasOwnProperty('lowercase') ? lowercaseCodes : []),
+    ...keyCodes(params, 'symbol')
+  ];
+}
+
+function keyCodes(params, type) {
+  return (Object.keys(params).filter(key => key.includes(type)).map(key => parseInt(key.split('-')[1])));
+}
+
+function range(start, stop) {
+  const numbers = [start];
+  let next = start;
+  while (next < stop) {
+    numbers.push(next += 1);
+  }
+
+  return numbers;
+}
